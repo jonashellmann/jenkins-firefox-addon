533dc61982522ed9aabd8798f00f9bbe3ac1f6b5
Jonas Hellmann <mail@jonas-hellmann.de>
2018-04-09 14:32:13 +0200

    Modified options page

--- options/options.js
+++ options/options.js
@@ -1,22 +1,29 @@
 var jobNumber = 0;
-var getSettings = browser.storage.local.get("settings");
-getSettings.then((res) => {
-	const {settings} = res;
-	document.querySelector("#baseurl").value = settings.baseurl;
-	for (job in settings.jobs) {
-		jobNumber = jobNumber + 1;
-		var input = document.createElement("input");
-		input.id = "job" + jobNumber;
-		input.type = "text";
-		input.value = job.name;
-		document.querySelector("#jobs").appendChild(input);
-	}
-});
+
+function restoreOptions() {
+	var getSettings = browser.storage.local.get("settings");
+	getSettings.then((res) => {
+		const {settings} = res;
+		document.querySelector("#baseurl").value = settings.baseurl;
+		document.querySelector("#updateinterval").value = settings.updateinterval;
+		for (i in settings.jobs) {
+			var job = settings.jobs[i];
+			jobNumber = jobNumber + 1;
+			var input = document.createElement("input");
+			input.id = "job" + jobNumber;
+			input.className = "jobinput";
+			input.type = "text";
+			input.value = job.name;
+			document.querySelector("#jobs").appendChild(input);
+		}
+	});	
+}
 
 function addJob(e) {
 	jobNumber = jobNumber + 1;
 	var input = document.createElement('input');
 	input.id = "job" + jobNumber;
+	input.className = "jobinput";
 	input.type = "text";
 	document.querySelector("#jobs").appendChild(input);
 }
@@ -28,16 +35,23 @@ function removeJob(e) {
 }
 
 function saveOptions(e) {
+	var baseurl = document.querySelector("#baseurl").value
+	if (baseurl.substr(-1) != '/') {
+        baseurl = baseurl + '/';
+    }
+	
 	var settings = {
 		settings: {
-			baseurl: document.querySelector("baseurl").value
-			jobs: [];
-		}
+			baseurl: baseurl,
+			updateinterval: parseInt(document.querySelector("#updateinterval").value, 10),
+			jobs: []
+		},
 	};
 
-	for (input in document.getElementsByClassName('job')) {
+	var jobinputs = document.getElementsByClassName("jobinput");
+	for(var i = 0; i < jobinputs.length; i++) {
 		settings.settings.jobs.push({
-			name: input.value;
+			name: jobinputs[i].value
 		});
 	}
 
@@ -49,6 +63,7 @@ function saveOptions(e) {
 	e.preventDefault();
 }
 
+restoreOptions();
 document.querySelector("form").addEventListener("submit", saveOptions);
 document.querySelector("#jobAdder").addEventListener("click", addJob);
 document.querySelector("#jobRemover").addEventListener("click", removeJob);
